619a20627fc4c6c1d628ccaa1ea29b80f0f2b714
Sankar Murugan <142787159+sankar2611@users.noreply.github.com>
2026-04-16 10:10:07 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
 export default defineConfig({
   plugins: [react()],
   server: { port: 5173 },
+  define: {
+    __APP_VERSION__: JSON.stringify('1.0.1'),
+  },
 });
